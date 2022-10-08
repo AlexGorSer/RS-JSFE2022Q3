d546abc20cce4294e627478c1e524fe69b21daf9
AlexGorSer <taskekaminari@gmail.com>
2022-10-09 01:08:43 +0200

    feat: add radio button logick

--- online-zoo/pages/main/sctipt.js
+++ online-zoo/pages/main/sctipt.js
@@ -1,17 +1,18 @@
 // Burger
 const burgerOpen = document.querySelector('.burger__menu'),
       burgerCloseButton = document.querySelector('.close__burger__menu'),
+      burgerBack = document.querySelector('.burger'),
       burgerWindow = document.querySelector('.burger__line');
 
 burgerOpen.addEventListener('click', () => {
     burgerWindow.classList.add('burger__on');
+    burgerBack.classList.add('burger__on');
 });
-burgerCloseButton.addEventListener('click', () => {
-    burgerWindow.classList.remove('burger__on')
-});
-burgerWindow.addEventListener('click', (e) => {
+
+burgerBack.addEventListener('click', (e) => {
     console.log(e.target)
-    if(e.target === burgerCloseButton || e.target ===burgerWindow) {
-        burgerWindow.classList.remove('burger__on')
+    if(e.target === burgerCloseButton || e.target ===burgerWindow || e.target === burgerBack) {
+        burgerWindow.classList.remove('burger__on');
+        burgerBack.classList.remove('burger__on');
     }
 })
