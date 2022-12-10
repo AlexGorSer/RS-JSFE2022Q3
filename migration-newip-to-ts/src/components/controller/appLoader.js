b803273954d35a6a4d358edd1aec7ab9372cf52d
AlexGorSer <taskekaminari@gmail.com>
2022-12-10 13:37:56 +0200

    feat: add news.ts

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://127.0.0.1:8075/', {
+    super('http://127.0.0.1:8075/', {
       apiKey: 'cae83f58930342f2b3b5f2e76849cadf', // получите свой ключ https://newsapi.org/
     });
   }
