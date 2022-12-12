b6e70986900bedd5944f1e521cb56615c23aa8d2
AlexGorSer <taskekaminari@gmail.com>
2022-12-12 17:21:42 +0200

    feat: add adaptive

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('http://127.0.0.1:8075/', {
+    super('https://nodenews.up.railway.app/', {
       apiKey: 'cae83f58930342f2b3b5f2e76849cadf', // получите свой ключ https://newsapi.org/
     });
   }
